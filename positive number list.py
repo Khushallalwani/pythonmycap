55e39adf06a36ef6bdc9b0c5279856f10307cf0f
Khushallalwani <89393214+Khushallalwani@users.noreply.github.com>
2023-06-21 21:07:29 +0530

    Add files via upload

--- positive number list.py
+++ positive number list.py
@@ -1,4 +1,10 @@
 list1=[11,-21,0,45,66,-93]
+list2=[12,14,-95,3]
+
 for i in list1:
     if i>=0:
-        print(i)
+        print(i,end=" ")
+
+for a in list2:
+    if a>=0:
+        print(a,end=" ")        
